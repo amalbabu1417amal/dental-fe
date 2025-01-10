19996f256d396eed7f78bb3d3a7378c686f4df08
amalbabu1417amal <amalab1417@gmail.com>
2025-01-10 05:10:22 -0800

    Fix image paths in HTML files

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -16,19 +16,19 @@ export class AboutComponent implements OnInit {
       name: 'Dr. Alex George', 
       role: 'Chief Dentist', 
       bio: 'Dr. Alex has over 15 years of experience in providing exceptional dental care.',
-      image: "../../../assets/img/male1.jpg" 
+      image: "assets/img/male1.jpg" 
     },
     { 
       name: 'Dr. Anu Robert', 
       role: 'Orthodontist', 
       bio: 'Dr. Anu  specializes in braces and dental alignment solutions.',
-      image: "../../../assets/img/female.jpg" 
+      image: "assets/img/female.jpg" 
     },
     { 
       name: 'Dr. Anjali D', 
       role: 'Orthodontist', 
       bio: 'Dr. Anjali specializes in braces and dental alignment solutions.',
-      image: "../../../assets/img/female2.jpg"
+      image: "assets/img/female2.jpg"
     },
 
   ];
@@ -37,27 +37,27 @@ export class AboutComponent implements OnInit {
     {
       name: 'General Dentistry',
       description: 'Routine checkups and cleanings to maintain your oral health.',
-      image: "../../../assets/img/cleaning.jpg"
+      image: "assets/img/cleaning.jpg"
     },
     {
       name: 'Cosmetic Dentistry',
       description: 'Enhance the appearance of your smile with veneers, whitening, and more.',
-      image: "../../../assets/img/tethbraces.jpg"
+      image: "assets/img/tethbraces.jpg"
     },
     {
       name: 'Orthodontics',
       description: 'Braces and aligners to straighten your teeth and improve your smile.',
-      image: "../../../assets/img/implaints.jpg" 
+      image: "assets/img/implaints.jpg" 
     },
     {
       name: 'Cosmetic Dentistry',
       description: 'Enhance the appearance of your smile with veneers, whitening, and more.',
-      image: "../../../assets/img/tethbraces.jpg"
+      image: "assets/img/tethbraces.jpg"
     },
     {
       name: 'Orthodontics',
       description: 'Braces and aligners to straighten your teeth and improve your smile.',
-      image: "../../../assets/img/implaints.jpg" 
+      image: "assets/img/implaints.jpg" 
     },
   ];
 }
